e6fe9996d912453e1c059bd76cad59edeb57cac3
Allen Philip John <allenphilip.john@jda.com>
2020-08-16 23:13:39 +0530

    Adding blog entry for creating new website using nodejs

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(express.static('public'));
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/images', express.static(__dirname + '/public/images'));
+app.use('/blog', express.static(__dirname + '/public/blog'));
 
 
 app.get('/', (req, res) => {
